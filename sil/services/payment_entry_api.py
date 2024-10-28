4d53a5b154b0d0fe51657e8be8dbd12680736b75
AdhiAdithyan <adhiadithyan95@gmail.com>
2024-10-28 18:06:36 +0530

    Latest updated code

--- sil/services/payment_entry_api.py
+++ sil/services/payment_entry_api.py
@@ -9,7 +9,17 @@ import traceback
 @frappe.whitelist(allow_guest=True)
 def getAllPaymentEntryDetails():
     frappe.clear_cache()
-    return frappe.get_all("Payment Entry",fields=["*"])
+
+    payment_details = frappe.get_all("Payment Entry",fields=["*"])
+
+    for payment in payment_details:
+        print(f"getAllPaymentEntryDetails payment:{payment}")
+        payment["payment_referrence"] = GetAllPaymentRefferenceByPaymentNumber(payment['name'])
+        for datails in payment:
+            print(f"getAllPaymentEntryDetails datails:{datails}")
+            print(f"getAllPaymentEntryDetails datails {datails}:{payment[datails]}")
+
+    return payment_details if payment_details else []
 
 
 @frappe.whitelist(allow_guest=True)
@@ -20,6 +30,17 @@ def AddNewPaymentEntry(payment_entry_no):
                 }
 
     return frappe.get_all("Payment Entry",filters=filters , fields=["*"])
+
+
+@frappe.whitelist(allow_guest=True)
+def GetAllPaymentRefferenceByPaymentNumber(payment_entry_no):
+    frappe.clear_cache()
+    filters = {
+                "parent": payment_entry_no
+                }
+
+    return frappe.get_all("Payment Entry Reference",filters=filters , fields=["*"])
+    # return frappe.get_all("Payment Entry Reference", fields=["*"])
 
 
 
